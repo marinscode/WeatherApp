d46428a68343ba0b4711a49943eeb8fc0eaecc46
Marin Totev <marin.totev@gmail.com>
2021-07-02 23:42:56 +0100

    Styling the UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchFormComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule
